5592cdec6fcf5c0f1085eb888d76801a40729017
= <quanva2521@gmail.com>
2021-12-17 17:31:14 +0700

    updated note

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -3,6 +3,7 @@ import { devToolsEnhancer } from "redux-devtools-extension";
 import reducer from "./bugs";
 
 export default function configureStore() {
+  // createStore là 1 function trả về state tiếp theo, cho state hiện tại và một action để xử lý
   const store = createStore(
     reducer,
     devToolsEnhancer({ trace: true })
